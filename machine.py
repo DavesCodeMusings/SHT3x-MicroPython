dbe8867a167711a8aa4e8dc11edfa2a8dcf17cfd
Dave Horton <dhorton668@outlook.com>
2025-04-17 12:17:59 -0500

    Add automated testing

---
+++ machine.py
@@ -0,0 +1,31 @@
+# Mock up for running tests under CPython
+sht3x_command_set = [
+    0x2400,  # Measure
+    0x3041,  # Clear status register
+    0x30A2,  # Soft reset
+    0xF32D   # Status
+]
+
+def Pin(gpio):
+    return gpio
+
+class SoftI2C:
+    def __init__(self, scl, sda):
+        print(f"Mock I2C created with clock={scl} and data={sda}")
+        pass
+
+    def writeto(self, addr, cmd):
+        print(f"Mock I2C writeto address={hex(addr)} command={cmd.hex()}")
+        acks = len(cmd)  # ACK sent for each byte received
+        if addr == 0x44 and int.from_bytes(cmd) in sht3x_command_set:
+            return acks
+        else:
+            return 0
+
+    def readfrom(self, addr, num_bytes):
+        if addr != 0x44:
+            assert OSError("ENODEV")
+        if num_bytes == 3:
+            return bytearray.fromhex("000081")  # Mockup of status_reg + CRC
+        elif num_bytes == 6:
+            return bytearray.fromhex("DEAD98BEEF92")  # Temp, CRC, Humid, CRC
--- run_tests.py
+++ run_tests.py
@@ -1,15 +1,83 @@
+# Run tests using mocked up sensor data. No fancy framework like
+# unittest or pytest, but it gets the job done.
+#
+# See also in this directory:
+# machine.py and micropython.py for helper code that mocks up
+# i2c and const() to work with CPython.
+
+from machine import Pin, SoftI2C
 from src.sht3x import SHT3x
-sht3x = SHT3x(None)
+
+i2c = SoftI2C(scl=Pin(22), sda=Pin(21))
+sht3x = SHT3x(i2c, addr=0x44, debug=True)
+sht3x_invalid_addr = SHT3x(i2c, addr=0x42, debug=True)
+
 
 def test_valid_crc8(sht3x):
     data = bytearray.fromhex("BEEF")  # Example given by SHT3x datasheet
     crc = sht3x._crc8(data)
     assert crc == bytearray.fromhex("92")
 
+
 def test_invalid_crc8(sht3x):
     data = bytearray.fromhex("BEEE")
     crc = sht3x._crc8(data)
     assert crc != bytearray.fromhex("92")
 
+
+def test_send_command(sht3x):
+    sht3x._send_command(0x30A2)
+
+
+def test_send_command_invalid_addr(sht3x_invalid_addr):
+    try:
+        sht3x_invalid_addr._send_command(0x30A2)
+    except OSError as ex:
+        exception = ex
+    assert str(exception) == "I2C command not acknowledged."
+
+
+def test_reset(sht3x):
+    sht3x.reset()
+
+
+def test_clear_status(sht3x):
+    sht3x.clear_status()
+
+
+def test_measure(sht3x):
+    sht3x.measure()
+
+
+def test_read(sht3x):
+    sht3x.read()
+
+
+def test_status(sht3x):
+    status = sht3x.status
+    print(status)
+
+
+def test_temperature(sht3x):
+    sht3x._raw_data = bytearray.fromhex("DEAD98BEEF92")
+    temperature = sht3x.temperature
+    assert temperature == 107.22
+
+
+def test_humidity(sht3x):
+    sht3x._raw_data = bytearray.fromhex("DEAD98BEEF92")
+    humidity = sht3x.humidity
+    assert humidity == 74.58
+
+
 test_valid_crc8(sht3x)
 test_invalid_crc8(sht3x)
+test_send_command(sht3x)
+test_send_command_invalid_addr(sht3x_invalid_addr)
+test_reset(sht3x)
+test_clear_status(sht3x)
+test_measure(sht3x)
+test_read(sht3x)
+test_status(sht3x)
+test_temperature(sht3x)
+test_humidity(sht3x)
